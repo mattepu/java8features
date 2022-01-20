22f1281a2a10d9f3273a187a9056e8daf3b1c17c
mattepu <mahesh.mattepu@ig.com>
2022-01-21 03:40:11 +0530

    removed sop

--- examples/src/com/java8/examples/streams/LoopAList.java
+++ examples/src/com/java8/examples/streams/LoopAList.java
@@ -7,14 +7,13 @@ import java.util.List;
  * Created by mattepm on 10-07-2018.
  */
 public class LoopAList {
-
     public static void main(String[] args) {
 
         List<String> list = Arrays.asList("Mahesh", "Sandeep", "Anand", "Ravi", "Biswajith");
         list.forEach(str -> System.out.println(str)); // just iterate the list whatever there
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
         list.forEach(str -> System.out.println(str.toUpperCase())); // you can apply any operations which are related to the String
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
         list.forEach(str -> {if(!str.equalsIgnoreCase("mahesh")) {
             System.out.println("Hi " + str);} //Filter the name mahesh from list
         });
